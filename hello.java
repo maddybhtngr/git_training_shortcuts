1947e848d338ea8b47aa0e37a43f34e6a8e53c12
maddybhtngr <maddybhtngr@gmail.com>
2020-02-02 10:53:50 +0530

    Added User Class

--- hello.java
+++ hello.java
@@ -1,5 +1,27 @@
 //Simple Java File to print out Hello World
-
+class User
+{
+  private String fname, lname;
+  private int age;
+  
+  User(String fname, String lname, int age)
+  {
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+  }
+  
+  public String getName(){return fname+" "+lname;}
+  public String getFirstName(){return fname;}
+  public String getLastName(){return lname;}
+  public String getAge(){return age;}
+  public void display()
+  {
+    System.out.println(getName());
+    System.out.println(getAge());
+  }
+  
+}
 
 class Hello
 {
